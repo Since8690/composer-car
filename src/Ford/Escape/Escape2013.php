fcdf8a16c03c6d124713a9e1abf928fb8a9c9515
unknown <tzhang@i9i8.com>
2017-09-04 09:41:25 +0800

    add files

---
+++ src/Ford/Escape/Escape2013.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 2017/9/4
+ * Time: 9:37
+ */
+namespace Ford\Escape;
+
+class Escape2013
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2013!<br />";
+    }
+}
